a6bdd22d5b87b2e3789411ceb604649c293765d1
KaviyaLabglo <kaviya.labglo@gmail.com>
2022-11-14 18:04:42 +0530

    Code Change

--- project/serializers.py
+++ project/serializers.py
@@ -1,8 +1,11 @@
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.password_validation import validate_password
+from rest_framework.validators import UniqueValidator
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework import serializers
 
-    
-    
-    
+
 # accounts/api/serializers.py
 
 from django.contrib.auth import authenticate
@@ -14,25 +17,7 @@ from rest_framework import serializers
 User._meta.get_field('email')._unique = True
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email')
 
-
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            validated_data['username'],
-            validated_data['email'],
-            validated_data['password']
-        )
-        return user
 
 
 class LoginSerializer(serializers.Serializer):
@@ -44,67 +29,63 @@ class LoginSerializer(serializers.Serializer):
         if user and user.is_active:
             return user
         raise serializers.ValidationError("Incorrect Credentials")
-    
-    
+
+
 class ProductSerializer((serializers.ModelSerializer)):
     class Meta:
         model = product
         fields = "__all__"
-        
-        
-    
-from rest_framework import serializers
-from django.contrib.auth.models import User
 
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.validators import UniqueValidator
-from django.contrib.auth.password_validation import validate_password
-from rest_framework.authtoken.models import Token
 
-#Serializer to Get User Details using Django Token Authentication
+# Serializer to Get User Details using Django Token Authentication
+
 class UserSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = User
-    fields = ["id", "first_name", "last_name", "username"]
+    class Meta:
+        model = User
+        fields = ["id", "first_name", "last_name", "username"]
 
 
-#Serializer to Register User
+# Serializer to Register User
 class RegisterSerializer(serializers.ModelSerializer):
-  email = serializers.EmailField(
-    required=True,
-    validators=[UniqueValidator(queryset=User.objects.all())]
-  )
-  password = serializers.CharField(
-    write_only=True, required=True, validators=[validate_password])
-  password2 = serializers.CharField(write_only=True, required=True)
-  class Meta:
-    model = User
-    fields = ('username', 'password', 'password2',
-         'email', 'first_name', 'last_name')
-    extra_kwargs = {
-      'first_name': {'required': True},
-      'last_name': {'required': True}
-    }
-  def validate(self, attrs):
-    if attrs['password'] != attrs['password2']:
-      raise serializers.ValidationError(
-        {"password": "Password fields didn't match."})
-    return attrs
-  def create(self, validated_data):
-    user = User.objects.create(
-      username=validated_data['username'],
-      email=validated_data['email'],
-      first_name=validated_data['first_name'],
-      last_name=validated_data['last_name']
-      
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
     )
-    user.set_password(validated_data['password'])
-    user.save()
-    print(user)
-    token, create = Token.objects.get_or_create(user=user)
-    print(token)
-    return user
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password2',
+                  'email', 'first_name', 'last_name')
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True}
+        }
+        token = serializers.SerializerMethodField('get_user_token')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+
+       # token, create = Token.objects.get_or_create(user=user)
+       # print(token)
+        # return user
         
-        
-        
+
+    def get_user_token(self, obj):
+        return Token.objects.get_or_create(user=obj)[0].key
--- project/views.py
+++ project/views.py
@@ -14,54 +14,12 @@ class UserDetail(generics.RetrieveAPIView):
     serializer_class = UserSerializer
     
     
-    
-    
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-class ExampleView(APIView):
-    permission_classes = [IsAuthenticated]
-    def get(self, request, format=None):
-        token, create = Token.objects.get_or_create(user=request.user)
-        content = {
-            'user': str(request.user),  # `django.contrib.auth.User` instance.
-            'auth': str(request.auth), 
-            'Token':str(token),
-            'user_id': str(request.user.id),
-        }
-        return Response(content)
-
-
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
-class CustomAuthToken(ObtainAuthToken):
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data,
-                                           context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-        print("Hiiiii")
-        return Response({
-            'token': token.key,
-            'user_id': user.pk,
-            'email': user.email
-        })
-        
+   
         
         
         
 
 
-# Get User API
-class UserAPI(generics.RetrieveAPIView):
-    permission_classes = [IsAuthenticated]
-    def get_object(self):
-        return self.request.user
-    
     
     
 from rest_framework import mixins
